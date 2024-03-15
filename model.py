c8500720e8de636fd479198ba4bee8e93221b273
Karyna Nechyporuk <63395637+KarynaNech@users.noreply.github.com>
2024-03-15 16:11:10 +0000

    FastAPI in model.py  updated

--- model.py
+++ model.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+import sentencepiece as spm
+import torch.nn as nn
+#from week2_Alex.ipynb import dataframe_sentence_generator, 
 
 app = FastAPI()
 
@@ -8,5 +11,13 @@ def read_root():
 
 @app.post("/get-prediction")
 def getPrediction(title):
-    #run funtion to call model
-    return None
+
+    #tokenize the title
+    sp = spm.SentencePieceProcessor()
+    sp.load('spm_Alex_week2.model')
+    sp.encode_as_pieces(title)
+
+    #2. Run the tokens through Word2Vec&take the average
+    
+    #3. Run the resulting method through our regression model
+    return prediction
